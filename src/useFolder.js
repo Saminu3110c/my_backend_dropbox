4c0f76da1bae6062fa63a3ab7ce7c90edebc661e
Saminu Isah <saminu08148556630@gmail.com>
2024-10-09 10:05:54 +0100

    Update

--- src/useFolder.js
+++ src/useFolder.js
@@ -9,7 +9,7 @@ const ACTIONS = {
   SET_CHILD_FILES: "set-child-files",
 }
 
-export const ROOT_FOLDER = { name: "Project", id: null, path: [] }
+export const ROOT_FOLDER = { name: "Dashboard", id: null, path: [] }
 
 function reducer(state, { type, payload }) {
   switch (type) {
